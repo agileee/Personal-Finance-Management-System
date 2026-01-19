b4307ae336eacd91ee87d11e6912335ff41adb90
Akhil <akhilgirish18@gmail.com>
2026-01-19 18:33:27 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,22 +1,30 @@
+import os 
 from flask import Flask
 from flask_mysqldb import MySQL
+
 import hashlib
 
-mysql = MySQL()  # Initialize MySQL
+mysql = MySQL()  
 
 def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your_secret_key'
+    app = Flask(
+    __name__,
+    static_folder="../static",    
+    static_url_path="/static"
+)
+
+    app.config['SECRET_KEY'] = 'secret_key'
 
     # MySQL Configuration
     app.config["MYSQL_HOST"] = "localhost"
-    app.config["MYSQL_USER"] = "your_user"
-    app.config["MYSQL_PASSWORD"] = "your_password"
+    app.config["MYSQL_USER"] = "root"
+    app.config["MYSQL_PASSWORD"] = "password"
     app.config["MYSQL_DB"] = "finance"
 
-    mysql.init_app(app)  # Initialize MySQL with the app
+    mysql.init_app(app)  
 
-    from .routes import main  # Import routes (Blueprint)
+    from .routes import main,api  
     app.register_blueprint(main)
+    app.register_blueprint(api)
 
     return app
